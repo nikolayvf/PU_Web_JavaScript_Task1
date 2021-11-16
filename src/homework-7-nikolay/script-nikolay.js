23e20296c3d94ba1751cbff328ad779c1d33e0b7
nikolayvf <nikolayvf@gmail.com>
2021-11-16 03:35:05 +0000

    Add files via upload

---
+++ src/homework-7-nikolay/script-nikolay.js
@@ -0,0 +1,35 @@
+        function checkImage(answer){
+            let text = '';
+            if(answer == 7){
+                console.log('Намерихте Уоли (Waldo)!');
+                text = 'Намерихте Уоли (Waldo)!';
+                document.getElementById("answer-box").style.backgroundColor="#CADC54";
+
+            }else{
+                console.log("Това не е Уоли (Waldo)! Опитайте пак!");
+                text = 'Това не е Уоли (Waldo)! Опитайте пак!';
+                document.getElementById("answer-box").style.backgroundColor="#F31B19";
+
+            }
+            document.getElementById("answer-box").innerHTML=text;
+        }
+
+        function checkQuizAnswer(answer){
+
+            switch(answer){
+                case 1:{
+                    document.getElementById("btn-a").style.backgroundColor='#F31B19';
+                }break;
+                case 2:{
+
+                }break;
+                case 3:{
+                    document.getElementById("btn-c").style.backgroundColor='#F31B19';
+                }break;
+                case 4:{
+                    document.getElementById("btn-d").style.backgroundColor='#F31B19';
+                }break;
+            }
+            document.getElementById("btn-b").style.backgroundColor='#F5AC00';
+            document.getElementById("btn-b").style.color='#532E07';
+        }
